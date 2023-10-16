a731c21f1efc1da42f49603f79bd1279a8b902c7
boogie1989 <sergeybugai@gmail.com>
2023-10-17 02:06:01 +0300

    refactoring

--- bot.py
+++ bot.py
@@ -3,9 +3,14 @@ def parse_input(user_input):
     cmd = cmd.strip().lower()
     return cmd, args
 
+def hello_command(args, contacts):
+    return "How can I help you?"
+
 def add_contact(args, contacts):
     if len(args) == 2:
         name, phone = args
+        if name in contacts:
+            return f"Contact {name} exists. For an update, use the 'change' command."
         contacts[name] = phone
         return "Contact added."
     else:
@@ -22,6 +27,17 @@ def change_contact(args, contacts):
     else:
         return "Invalid command. Please use 'change username phone'."
 
+def remove_contact(args, contacts):
+    if len(args) == 1:
+        name = args[0]
+        if name in contacts:
+            del contacts[name]
+            return f"Contact {name} deleted."
+        else:
+            return f"Contact {name} not found."
+    else:
+        return "Invalid command. Please use 'delete username'."
+
 def get_phone(args, contacts):
     if len(args) == 1:
         name = args[0]
@@ -32,12 +48,32 @@ def get_phone(args, contacts):
     else:
         return "Invalid command. Please use 'phone username'."
 
-def show_all(contacts):
+def show_all(args, contacts):
+    contact_strings = []
     if contacts:
         for name, phone in contacts.items():
-            print(f"{name}: {phone}")
+            contact_strings.append(f"{name}: {phone}")
     else:
-        print("No contacts found.")
+        contact_strings.append("No contacts found.")
+    return contact_strings
+
+COMMAND_HELLO = "hello"
+COMMAND_ADD = "add"
+COMMAND_CHANGE = "change"
+COMMAND_REMOVE = "remove"
+COMMAND_PHONE = "phone"
+COMMAND_ALL = "all"
+COMMAND_CLOSE = "close"
+COMMAND_EXIT = "exit"
+
+command_functions = {
+    COMMAND_HELLO: hello_command,
+    COMMAND_ADD: add_contact,
+    COMMAND_CHANGE: change_contact,
+    COMMAND_REMOVE: remove_contact,
+    COMMAND_PHONE: get_phone,
+    COMMAND_ALL: show_all,
+}
 
 def main():
     contacts = {}
@@ -46,21 +82,18 @@ def main():
         user_input = input("Enter a command: ")
         command, args = parse_input(user_input)
 
-        if command in ["close", "exit"]:
+        if command in [COMMAND_CLOSE, COMMAND_EXIT]:
             print("Good bye!")
             break
-        elif command == "hello":
-            print("How can I help you?")
-        elif command == "add":
-            print(add_contact(args, contacts))
-        elif command == "change":
-            print(change_contact(args, contacts))
-        elif command == "phone":
-            print(get_phone(args, contacts))
-        elif command == "all":
-            show_all(contacts)
+        elif command in command_functions:
+            print(command_functions[command](args, contacts))
         else:
             print("Invalid command.")
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
